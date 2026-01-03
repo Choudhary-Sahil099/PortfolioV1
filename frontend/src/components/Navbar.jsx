db1b893f3d2147d8c8170a4ba4adbc300878a1d0
Sahil choudhary <freedomchoudhary099@gmail.com>
2026-01-03 20:47:04 +0530

    Fix:image visibility of the profile picture in the github

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,13 +5,13 @@ import { TiThMenu } from "react-icons/ti";
 
 const Navbar = ({ setOpen }) => {
   return (
-    <div className="flex justify-between items-center px-20 pt-10 pb-2 bg-[#F9E6FF] sticky top-0 z-50">
+    <div className="navWraper">
       <img className="h-14 w-16" src={Logo} alt="logo" />
 
       <div className="flex items-center gap-4">
         <a
           aria-label="Open Resume"
-          className="bg-[#5419D7] h-14 w-16 flex items-center justify-center rounded-xl hover:bg-[#7342de] cursor-pointer"
+          className="nav-Btn"
           href=""
         >
           <img className="h-10 w-10" src={resume} alt="resume" />
@@ -20,9 +20,9 @@ const Navbar = ({ setOpen }) => {
         <button
           aria-label="Open Menu"
           onClick={() => setOpen(true)}
-          className="bg-[#5419D7] h-14 w-16 flex items-center justify-center rounded-xl hover:bg-[#7342de] cursor-pointer"
+          className="nav-Btn"
         >
-          <TiThMenu className="h-10 w-12 text-white" />
+          <TiThMenu className="nav-BtnImg text-white" />
         </button>
       </div>
     </div>
--- frontend/src/components/profile.jsx
+++ frontend/src/components/profile.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import { IoCall } from "react-icons/io5";
 const Profile = () => {
   return (
-    <section className="bg-[#F9E6FF] min-h-[87vh] w-full flex flex-row px-20 gap-20" id="profile">
-        <div className="pt-17 flex flex-col gap-6">
+    <section className="profile-Wrapper" id="profile">
+        <div className="introText">
           <p className="text-2xl">
             <span className="text-[#5419D7]">Hello</span> There!
           </p>
@@ -19,14 +19,14 @@ const Profile = () => {
             scalable applications and solving real-world challenges through
             clean, analytical code.
           </p>
-          <button className="bg-[#5419D7] text-white w-45 h-15 rounded-xl text-2xl hover:bg-[#7342de] cursor-pointer mt-6 flex justify-center items-center gap-2" onClick={() => {
+          <button className="contact-btn" onClick={() => {
             document.getElementById("contact")?.scrollIntoView();
           }}>
             <div>Contact Me</div> <IoCall />
           </button>
         </div>
-        <div className="flex items-center justify-center">
-          <img className="h-[65%] rounded-full object-contai" src="/api/profile-image" alt="profile" />
+        <div className="imageDiv">
+          <img className="profilePic" src="/api/profile-image" alt="profile" />
         </div>
       </section>
   );
